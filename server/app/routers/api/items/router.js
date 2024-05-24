1ea172bd57e980c04e74a11d464b01f0dd5b8a8a
alexis.guillon <alexis.guillon44@orange.fr>
2024-05-24 16:21:18 +0200

    Harmonia 03

--- server/app/routers/api/items/router.js
+++ server/app/routers/api/items/router.js
@@ -2,9 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
 
 // Import item-related actions
 const { browse, read, add } = require("../../../controllers/itemActions");
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,13 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
-
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const { sayWelcome } = require("../../controllers/sayAction");
+
+router.get("/", sayWelcome);
+
+const programs = require("./programs/router");
+
+router.use("/programs", programs);
 
 /* ************************************************************************* */
 
